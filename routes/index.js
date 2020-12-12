4e4c9a666015ef2a039f1a1244d3accb8296caca
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-12-12 15:02:32 +0700

    modify navbar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Colab Code' });
+  res.render('index', { title: 'Colab Code - Realtime Pair Coding' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Colab Code - Realtime Pair Coding' });
 });
 
 module.exports = router;
