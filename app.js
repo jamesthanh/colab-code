cc7c60181355a75f65e59b81e8462796bf5fb3e9
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-12-12 15:57:09 +0700

    add form submit

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var expressValidator = require('express-validator')
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,6 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(expressValidator())
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,28 @@ router.get('/about', function(req, res, next) {
   res.render('about', { title: 'Colab Code - Realtime Pair Coding' });
 });
 
+router.route('/contact')
+  .get(function(req, res, next) {
+    res.render('contact', {title: 'Colab Code - Realtime Pair Coding'})
+  })
+  .post(function(req, res, next) {
+    req.checkBody('name', 'Invalid name').notEmpty();
+    req.checkBody('email', 'Invalid email').isEmail();
+    req.checkBody('message', 'Empty message').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if(errors) {
+      res.render('contact', {
+        title: 'Code4Share - a platform for sharing code.',
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message,
+        errorMessages: errors
+      });
+    } else {
+      res.render('thank', { title: 'Code4Share - a platform for sharing code.'});
+    }
+  })
+
 module.exports = router;
